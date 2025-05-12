79397e2f7daa64b38716b1f82e0be0bb74bcd8f4
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-05-12 23:13:28 +0530

    added findAll() & findByTitleContaining()

--- spring-boot-rest-api/src/main/java/com/example/spring_boot_rest_api/controller/TutorialController.java
+++ spring-boot-rest-api/src/main/java/com/example/spring_boot_rest_api/controller/TutorialController.java
@@ -1,4 +1,37 @@
 package com.example.spring_boot_rest_api.controller;
+import java.util.ArrayList;
+import java.util.List;
+import com.example.spring_boot_rest_api.model.Tutorial;
+import com.example.spring_boot_rest_api.service.TutorialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins="http://localhost:8081")
+@RestController
+@RequestMapping("/api")
 public class TutorialController {
+    @Autowired
+    TutorialService tutorialService;
+
+    @GetMapping("/tutorials")
+    public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title){
+        try{
+            List<Tutorial> tutorials=new ArrayList<>();
+
+            if(title==null)
+                tutorialService.findAll().forEach(tutorials::add);
+            else
+                tutorialService.findByTitleContaining(title).forEach(tutorials::add);
+
+            if(tutorials.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch(Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- spring-boot-rest-api/src/main/java/com/example/spring_boot_rest_api/service/TutorialService.java
+++ spring-boot-rest-api/src/main/java/com/example/spring_boot_rest_api/service/TutorialService.java
@@ -1,4 +1,19 @@
 package com.example.spring_boot_rest_api.service;
 
+import com.example.spring_boot_rest_api.model.Tutorial;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TutorialService {
+    static List<Tutorial> tutorials=new ArrayList<>();
+    static long id=0;
+
+    public List<Tutorial> findAll(){
+        return tutorials;
+    }
+
+    public List<Tutorial> findByTitleContaining(String title){
+        return tutorials.stream().filter(tutorial -> tutorial.getTitle().contains(title)).toList();
+    }
 }
