335350384b59d0808510259d38553bcc432bdb2b
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-05-13 00:42:56 +0530

    added funcs in TutorialService

--- spring-boot-rest-api/src/main/java/com/example/spring_boot_rest_api/service/TutorialService.java
+++ spring-boot-rest-api/src/main/java/com/example/spring_boot_rest_api/service/TutorialService.java
@@ -1,7 +1,7 @@
 package com.example.spring_boot_rest_api.service;
 
 import com.example.spring_boot_rest_api.model.Tutorial;
-
+import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,5 +15,43 @@ public class TutorialService {
 
     public List<Tutorial> findByTitleContaining(String title){
         return tutorials.stream().filter(tutorial -> tutorial.getTitle().contains(title)).toList();
+    }
+
+    public Tutorial findById(long id){
+        return tutorials.stream().filter(tutorial -> id == tutorial.getId()).findAny().orElse(null);
+    }
+
+    public Tutorial save(Tutorial tutorial){
+        //update Tutorial
+        if(tutorial.getId()!=0){
+            long _id=tutorial.getId();
+
+            for (int idx=0; idx< tutorials.size();idx++){
+                if(_id==tutorials.get(idx).getId()){
+                    tutorials.set(idx,tutorial);
+                    break;
+                }
+            }
+
+            return tutorial;
+        }
+
+        //create new Tutorial
+        tutorial.setId(++id);
+        tutorials.add(tutorial);
+
+        return tutorial;
+    }
+
+    public void deleteById(long id){
+        tutorials.removeIf(tutorial -> id==tutorial.getId());
+    }
+
+    public void deleteAll(){
+        tutorials.removeAll(tutorials);
+    }
+
+    public List<Tutorial> findByPublished(boolean isPublished) {
+        return tutorials.stream().filter(tutorial -> isPublished==tutorial.isPublished()).toList();
     }
 }
